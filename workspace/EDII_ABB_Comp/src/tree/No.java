161d1583c1fe059e12eb1d88b71d9593cc72ade4
Rafael Tavares Andrade <rafaeltavaresandrade@gmail.com>
2015-03-25 17:19:17 -0300

    Novo projeto

---
+++ workspace/EDII_ABB_Comp/src/tree/No.java
@@ -0,0 +1,17 @@
+package tree;
+
+public class No {
+	private Aluno aluno;
+
+	public No(Aluno aluno) {
+		this.aluno = aluno;
+	}
+
+	public Aluno getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+}
